20bd470011e25cfbcd00e4756b94a718bf40c96d
AleksandreMoists <aleksandremoistsrapishvili@gmail.com>
2025-01-27 23:24:13 +0400

    Add responsivity & image hovering & button & title

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -6,6 +6,7 @@ import { projectsData } from '../../utils/data';
 import { Button } from '@mui/material';
 import Aos from 'aos';
 import RightUpSvg from '../../assets/svgs/ProjectPageSvgs/RightUpSvg';
+import NextSvg from '../../assets/svgs/ProjectPageSvgs/NextSvg';
 
 
 const Projects = () => {
@@ -20,30 +21,44 @@ const Projects = () => {
             </div>
 
             {projectsData.map((project) => (
+                <React.Fragment key={project.id}>
+                <hr />
                 <div className={styles.projects} key={project.id} data-aos="fade-up">
-                    {/* <hr></hr> */}
                     <div className={styles.projectTypography}>
                         <span>
                             <Typography variant='h3'>Software Development</Typography>
                         </span>
                         <span>
-                            <Typography variant='h3'>{project.title}</Typography>
+                            <Typography variant='h3' className={styles.title}>{project.title}</Typography>
                         </span>
                     </div>
 
                     <div className={styles.projectsContent}> 
+                        <div style={{ marginTop: '-0.5rem' }} className={styles.projectImageWrapper}>
+                            <div className={styles.projectImage}>
+                                <img src={project.image} alt={project.title} className={styles.image} />
+                                <div className={styles.content}>
+                                    <h1>Image hover effect</h1>
+                                    <p>Lorem Ipsum dolor sit amet</p>
+                                </div>
+                            </div>
+                        </div>
                         <span>
-                            <img src={project.image} alt={project.title} className={styles.projectsImage} />
+                        <Button 
+                            variant='contained'
+                            startIcon={<NextSvg />}
+                            classes={{
+                                root: styles.root,
+                                label: styles.label,
+                                startIcon: styles.startIcon
+                            }}
+                            >
+                            See More
+                            </Button>
                         </span>
-                        <span className={styles.projectButoon}>
-                            <Button variant='contained' startIcon={<RightUpSvg />} className={styles.primaryButton}> See More </Button>
-                        </span>
-                    </div>
-
-                    <div> 
-                        
                     </div>
                 </div>
+                </React.Fragment>
             ))}
 
         </div>
