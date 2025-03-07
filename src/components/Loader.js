495bc31e4ef0c933aeaa8e0afc6a0f4b292fc787
AleksandreMoists <aleksandremoistsrapishvili@gmail.com>
2025-03-08 01:35:00 +0400

    Customize loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,17 +1,13 @@
 // Loader.js
-import React from 'react';
-import '../styles/Loader.css'; // for your animation styling
-
 function Loader() {
-  return (
-    <div className="loader-container">
-      {/* Add your initials in an animated way */}
-      <div className="loader-initials">
-        {/* Could be an SVG, or simple text that you style */}
-        <span>AB</span>
+    return (
+      <div className="loader-container">
+        <div className="loader-initials">
+          <span className="letter-a">A</span>
+          <span className="letter-m">M</span>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default Loader;
+export default Loader
